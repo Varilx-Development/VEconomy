f12f36c94b9e4294d20a2ee719300976d52d2b52
Shadow <nicolafabian75@gmail.com>
2024-12-31 01:57:35 +0100

    initial commit

---
+++ src/main/java/de/varilx/veconomy/VEconomy.java
@@ -0,0 +1,17 @@
+package de.varilx.veconomy;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class VEconomy extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
