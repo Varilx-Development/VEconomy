46f003bbdbaa2461765953e5218b102a9ed970a2
VAXShadow1929 <193131876+VAXShadow1929@users.noreply.github.com>
2024-12-31 10:58:07 +0100

    Add MoneyCommand class to handle economy actions
    Introduce a new MoneyCommand class for the VEconomy plugin to allow players to check their balance and execute payments. Includes support for tab completion and basic validation for arguments provided by players.

---
+++ src/main/java/de/varilx/veconomy/commands/MoneyCommand.java
@@ -0,0 +1,143 @@
+package de.varilx.veconomy.commands;
+
+
+import de.varilx.command.VaxCommand;
+import de.varilx.database.repository.Repository;
+import de.varilx.utils.MathUtils;
+import de.varilx.utils.NumberUtils;
+import de.varilx.utils.language.LanguageUtils;
+import de.varilx.veconomy.VEconomy;
+import de.varilx.veconomy.transaction.EconomyTransaction;
+import de.varilx.veconomy.transaction.type.TransactionType;
+import de.varilx.veconomy.user.EconomyUser;
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * Project: VEconomy
+ * Package: de.varilx.veconomy.commands
+ * <p>
+ * Author: ShadowDev1929
+ * Created on: 31.12.2024
+ */
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class MoneyCommand extends VaxCommand {
+
+    VEconomy plugin;
+    Repository<EconomyUser, UUID> repository;
+
+    public MoneyCommand(@NotNull String name, VEconomy plugin) {
+        super(name);
+        this.plugin = plugin;
+        this.repository = (Repository<EconomyUser, UUID>) plugin.getDatabaseService().getRepository(EconomyUser.class);
+    }
+
+    @Override
+    public boolean execute(@NotNull CommandSender sender, @NotNull String commandLabel, @NotNull String[] args) {
+        if(sender instanceof Player player) {
+            if(args.length == 0) {
+                repository.findFirstById(player.getUniqueId()).thenAccept(user -> {
+                   player.sendMessage(LanguageUtils.getMessage("command_money_own_balance",
+                           Placeholder.parsed("balance", MathUtils.formatNumber(user.getBalance())),
+                           Placeholder.parsed("currency_name", LanguageUtils.getMessageString("currency_name"))
+                   ));
+                });
+            } else if(args.length == 1) {
+                if(args[0].toLowerCase().contentEquals("top")) {
+
+                }
+            } else if(args.length == 3) {
+                if(args[0].toLowerCase().contentEquals("pay")) {
+                    String user = args[1];
+
+                    if(!NumberUtils.isNumeric(args[2])) {
+                        player.sendMessage(LanguageUtils.getMessage("no_valid_number"));
+                        return false;
+                    }
+
+                    double amount = Double.parseDouble(args[2]);
+
+                    repository.findByFieldName("name", user).thenAccept(target -> {
+                        if(target == null) {
+                            player.sendMessage(LanguageUtils.getMessage("user_not_found"));
+                            return;
+                        }
+                        if(player.getUniqueId().equals(target.getUniqueId())) {
+                            player.sendMessage(LanguageUtils.getMessage("command_money_pay_self"));
+                            return;
+                        }
+
+                        repository.findFirstById(player.getUniqueId()).thenAccept(senderUser -> {
+                            if(senderUser.getBalance() < amount) {
+                                player.sendMessage(LanguageUtils.getMessage("command_money_pay_not_enough_money"));
+                                return;
+                            }
+
+                            senderUser.removeBalance(amount);
+                            target.addBalance(amount);
+
+                            EconomyTransaction senderTransaction = new EconomyTransaction();
+                            senderTransaction.setAccountId(senderUser.getUniqueId());
+                            senderTransaction.setTime(System.currentTimeMillis());
+                            senderTransaction.setAmount(amount);
+                            senderTransaction.setType(TransactionType.PAY);
+                            senderTransaction.setReceiverId(target.getUniqueId());
+                            senderTransaction.setUser(senderUser);
+
+                            senderUser.addTransaction(senderTransaction);
+                            repository.save(senderUser);
+
+                            EconomyTransaction receiverTransaction = new EconomyTransaction();
+                            receiverTransaction.setAccountId(target.getUniqueId());
+                            receiverTransaction.setTime(System.currentTimeMillis());
+                            receiverTransaction.setAmount(amount);
+                            receiverTransaction.setType(TransactionType.PAY_RECEIVE);
+                            receiverTransaction.setReceiverId(senderUser.getUniqueId());
+                            receiverTransaction.setUser(target);
+
+                            target.addTransaction(receiverTransaction);
+
+                            repository.save(target);
+
+                            player.sendMessage(LanguageUtils.getMessage("command_money_pay_success",
+                                    Placeholder.parsed("receiver", target.getName()),
+                                    Placeholder.parsed("amount", MathUtils.formatNumber(amount)),
+                                    Placeholder.parsed("currency_name", LanguageUtils.getMessageString("currency_name"))
+                            ));
+
+                            Player receiver = plugin.getServer().getPlayer(target.getUniqueId());
+                            if(receiver == null) return;
+
+                            receiver.sendMessage(LanguageUtils.getMessage("command_money_pay_success_receiver",
+                                    Placeholder.parsed("sender", senderUser.getName()),
+                                    Placeholder.parsed("amount", MathUtils.formatNumber(amount)),
+                                    Placeholder.parsed("currency_name", LanguageUtils.getMessageString("currency_name"))
+                            ));
+
+                        });
+
+                    });
+                }
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public @NotNull List<String> tabComplete(@NotNull CommandSender sender, @NotNull String alias, @NotNull String[] args) throws IllegalArgumentException {
+        if(args.length == 1) {
+            return List.of(
+                    LanguageUtils.getMessageString("Commands.Money.Arguments.Top"),
+                    LanguageUtils.getMessageString("Commands.Money.Arguments.Pay")
+            );
+        }
+        return super.tabComplete(sender, alias, args);
+    }
+}
