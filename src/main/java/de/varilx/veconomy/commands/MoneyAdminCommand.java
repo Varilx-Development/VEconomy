91a1f3a2c6b345cd245ff13d5490c4e515a7fc10
VAXShadow1929 <193131876+VAXShadow1929@users.noreply.github.com>
2025-01-01 15:12:25 +0100

    Add admin commands for managing user balances and transactions
    Implemented commands to add, remove, set, and reset user balances, as well as view user transactions via a GUI. Improved command usage feedback and tab completion for better admin usability.

--- src/main/java/de/varilx/veconomy/commands/MoneyAdminCommand.java
+++ src/main/java/de/varilx/veconomy/commands/MoneyAdminCommand.java
@@ -3,11 +3,16 @@ package de.varilx.veconomy.commands;
 
 import de.varilx.command.VaxCommand;
 import de.varilx.database.repository.Repository;
+import de.varilx.utils.NumberUtils;
 import de.varilx.utils.language.LanguageUtils;
 import de.varilx.veconomy.VEconomy;
+import de.varilx.veconomy.gui.TransactionsGui;
+import de.varilx.veconomy.transaction.EconomyTransaction;
+import de.varilx.veconomy.transaction.type.TransactionType;
 import de.varilx.veconomy.user.EconomyUser;
 import lombok.AccessLevel;
 import lombok.experimental.FieldDefaults;
+import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
@@ -43,6 +48,46 @@ public class MoneyAdminCommand extends VaxCommand {
                 player.sendMessage(LanguageUtils.getMessage("no_permission"));
                 return false;
             }
+
+            if(args.length == 0) {
+                LanguageUtils.getMessageList("Commands.MoneyAdmin.Usage").forEach(player::sendMessage);
+            } else if(args.length == 2) {
+                if(args[0].toLowerCase().contentEquals(LanguageUtils.getMessageString("Commands.MoneyAdmin.Arguments.Reset"))) {
+                    handleReset(player, args[1]);
+                } else if(args[0].toLowerCase().contentEquals(LanguageUtils.getMessageString("Commands.MoneyAdmin.Arguments.Transactions"))) {
+                    handleTransactions(player, args[1]);
+                }
+            } else if(args.length == 3) {
+                if(args[0].toLowerCase().contentEquals(LanguageUtils.getMessageString("Commands.MoneyAdmin.Arguments.Add"))) {
+                    String user = args[1];
+
+                    if(!NumberUtils.isNumeric(args[2])) {
+                        player.sendMessage(LanguageUtils.getMessage("no_valid_number"));
+                        return false;
+                    }
+                    double amount = Double.parseDouble(args[2]);
+                    handleAdd(player, user, amount);
+                } else if(args[0].toLowerCase().contentEquals(LanguageUtils.getMessageString("Commands.MoneyAdmin.Arguments.Remove"))) {
+                    String user = args[1];
+
+                    if(!NumberUtils.isNumeric(args[2])) {
+                        player.sendMessage(LanguageUtils.getMessage("no_valid_number"));
+                        return false;
+                    }
+                    double amount = Double.parseDouble(args[2]);
+                    handleRemove(player, user, amount);
+                } else if(args[0].toLowerCase().contentEquals(LanguageUtils.getMessageString("Commands.MoneyAdmin.Arguments.Set"))) {
+                    String user = args[1];
+
+                    if(!NumberUtils.isNumeric(args[2])) {
+                        player.sendMessage(LanguageUtils.getMessage("no_valid_number"));
+                        return false;
+                    }
+                    double amount = Double.parseDouble(args[2]);
+                    handleSet(player, user, amount);
+                }
+            }
+
         }
 
         return false;
@@ -50,6 +95,128 @@ public class MoneyAdminCommand extends VaxCommand {
 
     @Override
     public @NotNull List<String> tabComplete(@NotNull CommandSender sender, @NotNull String alias, @NotNull String[] args) throws IllegalArgumentException {
+        if(sender instanceof Player player && player.hasPermission(LanguageUtils.getMessageString("Commands.MoneyAdmin.Permission"))) {
+            return List.of(
+                    LanguageUtils.getMessageString("Commands.MoneyAdmin.Arguments.Add"),
+                    LanguageUtils.getMessageString("Commands.MoneyAdmin.Arguments.Remove"),
+                    LanguageUtils.getMessageString("Commands.MoneyAdmin.Arguments.Set"),
+                    LanguageUtils.getMessageString("Commands.MoneyAdmin.Arguments.Reset"),
+                    LanguageUtils.getMessageString("Commands.MoneyAdmin.Arguments.Transactions")
+            );
+        }
         return super.tabComplete(sender, alias, args);
     }
+
+    private void handleRemove(Player player, String user, double amount) {
+        repository.findByFieldName("name", user).thenAccept(target -> {
+            if(target == null) {
+                player.sendMessage(LanguageUtils.getMessage("user_not_found"));
+                return;
+            }
+
+            if((target.getBalance() - amount) < 0) {
+                player.sendMessage(LanguageUtils.getMessage("command_moneyadmin_remove_not_enough_money"));
+                return;
+            }
+
+            target.removeBalance(amount);
+            EconomyTransaction transaction = new EconomyTransaction();
+            transaction.setAccountId(target.getUniqueId());
+            transaction.setTime(System.currentTimeMillis());
+            transaction.setAmount(amount);
+            transaction.setType(TransactionType.ADMIN_REMOVE);
+            transaction.setReceiverId(target.getUniqueId());
+            transaction.setUser(target);
+            target.addTransaction(transaction);
+            repository.save(target);
+            player.sendMessage(LanguageUtils.getMessage("command_moneyadmin_remove_success",
+                    Placeholder.parsed("balance", String.valueOf(amount)),
+                    Placeholder.parsed("currency_name", LanguageUtils.getMessageString("currency_name"))
+            ));
+        });
+    }
+
+    private void handleSet(Player player, String user, double amount) {
+        repository.findByFieldName("name", user).thenAccept(target -> {
+            if(target == null) {
+                player.sendMessage(LanguageUtils.getMessage("user_not_found"));
+                return;
+            }
+
+            target.setBalance(amount);
+            EconomyTransaction transaction = new EconomyTransaction();
+            transaction.setAccountId(target.getUniqueId());
+            transaction.setTime(System.currentTimeMillis());
+            transaction.setAmount(amount);
+            transaction.setType(TransactionType.ADMIN_SET);
+            transaction.setReceiverId(target.getUniqueId());
+            transaction.setUser(target);
+            target.addTransaction(transaction);
+            repository.save(target);
+            player.sendMessage(LanguageUtils.getMessage("command_moneyadmin_set_success",
+                    Placeholder.parsed("balance", String.valueOf(amount)),
+                    Placeholder.parsed("currency_name", LanguageUtils.getMessageString("currency_name"))
+            ));
+        });
+    }
+
+    private void handleAdd(Player player, String user, double amount) {
+        repository.findByFieldName("name", user).thenAccept(target -> {
+            if(target == null) {
+                player.sendMessage(LanguageUtils.getMessage("user_not_found"));
+                return;
+            }
+
+            target.addBalance(amount);
+            EconomyTransaction transaction = new EconomyTransaction();
+            transaction.setAccountId(target.getUniqueId());
+            transaction.setTime(System.currentTimeMillis());
+            transaction.setAmount(amount);
+            transaction.setType(TransactionType.ADMIN_ADD);
+            transaction.setReceiverId(target.getUniqueId());
+            transaction.setUser(target);
+            target.addTransaction(transaction);
+            repository.save(target);
+
+            player.sendMessage(LanguageUtils.getMessage("command_moneyadmin_add_success",
+                    Placeholder.parsed("balance", String.valueOf(amount)),
+                    Placeholder.parsed("currency_name", LanguageUtils.getMessageString("currency_name"))
+            ));
+        });
+    }
+
+    private void handleReset(Player player, String user) {
+        repository.findByFieldName("name", user).thenAccept(target -> {
+            if(target == null) {
+                player.sendMessage(LanguageUtils.getMessage("user_not_found"));
+                return;
+            }
+
+            target.setBalance(0);
+
+            EconomyTransaction transaction = new EconomyTransaction();
+            transaction.setAccountId(target.getUniqueId());
+            transaction.setTime(System.currentTimeMillis());
+            transaction.setAmount(0);
+            transaction.setType(TransactionType.ADMIN_RESET);
+            transaction.setReceiverId(target.getUniqueId());
+            transaction.setUser(target);
+            target.addTransaction(transaction);
+            repository.save(target);
+            player.sendMessage(LanguageUtils.getMessage("command_moneyadmin_reset_success",
+                    Placeholder.parsed("username", user)));
+        });
+    }
+
+    private void handleTransactions(Player player, String user) {
+        repository.findByFieldName("name", user).thenAccept(target -> {
+            if(target == null) {
+                player.sendMessage(LanguageUtils.getMessage("user_not_found"));
+                return;
+            }
+
+            new TransactionsGui(player, target.getUniqueId(), plugin);
+        });
+    }
+
 }
